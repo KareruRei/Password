f96703d05149c19ae2e96663c84631f312f599f4
KareruRei <karerurei@gmail.com>
2026-04-03 00:14:14 +0800

    api changes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BASE = import.meta?.env?.VITE_API_BASE || "http://127.0.0.1:8000";
+
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: BASE,
   headers: { "Content-Type": "application/json" },
 });
 
